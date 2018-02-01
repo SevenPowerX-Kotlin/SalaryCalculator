f47d2f48fef84a4fefb5d4891f247672c05312fb
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2018-02-01 20:03:40 +0100

    Created in AndroidStudio the project  Salary Calculator

---
+++ app/src/main/java/ua/com/sevenpowerx/android/salarycalculator/MainActivity.kt
@@ -0,0 +1,12 @@
+package ua.com.sevenpowerx.android.salarycalculator
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
